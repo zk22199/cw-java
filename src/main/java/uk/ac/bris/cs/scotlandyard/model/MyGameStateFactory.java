72709ab2a243c3359dad17a6899f455c00d7febe
zk22199 <zk22199@bristol.ac.uk>
2023-03-12 20:49:44 +0000

    Reshuffled code so that it follows coursework guide (16/22)

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -1,14 +1,13 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
-
-import javax.annotation.Nonnull;
-
 import com.google.common.collect.ImmutableSet;
+import java.util.*;
+import javax.annotation.Nonnull;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
-
-import java.util.Optional;
+import uk.ac.bris.cs.scotlandyard.model.Move.*;
+import uk.ac.bris.cs.scotlandyard.model.Piece.*;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
 
 /**
  * cw-model
@@ -16,27 +15,89 @@ import java.util.Optional;
  */
 public final class MyGameStateFactory implements Factory<GameState> {
 
+	private final class MyGameState implements GameState {
+
+		// LOCAL VARIABLES:
+		private GameSetup setup;
+		private ImmutableSet<Piece> remaining;
+		private ImmutableList<LogEntry> log;
+		private Player mrX;
+		private List<Player> detectives;
+		private ImmutableSet<Move> moves;
+		private ImmutableSet<Piece> winner;
+
+
+		// CONSTRUCTOR:
+		private MyGameState(
+				final GameSetup setup,
+				final ImmutableSet<Piece> remaining,
+				final ImmutableList<LogEntry> log,
+				final Player mrX,
+				final List<Player> detectives)
+		{
+			if (setup != null) this.setup = setup; else throw new IllegalArgumentException();
+			this.remaining = remaining;
+			this.log = log;
+			if (mrX != null) this.mrX = mrX; else throw new NullPointerException();
+			if (mrX.isDetective()) throw new IllegalArgumentException();
+			if (!detectives.contains(null)) this.detectives = detectives; else throw new NullPointerException();
+			this.winner = ImmutableSet.of(); 				// make winners initially empty;
+			if(setup.moves.isEmpty()) throw new IllegalArgumentException("Moves is empty!");
+
+			for (Player detective : detectives) { 								// check ticket allocation
+				if (detective.has(ScotlandYard.Ticket.SECRET) || detective.has(ScotlandYard.Ticket.DOUBLE)) throw new IllegalArgumentException();
+			}
+			for (int i = 0; i < detectives.size(); i++){						// iterate over detectives to find duplicates/overlaps
+				for (int j = i+1; j < detectives.size(); j++){					// start at j=i+1 to avoid more than one check between each detective
+					if (detectives.get(i).equals(detectives.get(j)) || detectives.get(i).location() == detectives.get(j).location()) throw new IllegalArgumentException();
+				}
+			}
+
+		}
+
+
+		// IMPLEMENTATION METHODS:
+		@Override public GameSetup getSetup() {  return setup; }
+		@Override  public ImmutableSet<Piece> getPlayers() { return null; }
+
+		@Nonnull @Override
+		public Optional<Integer> getDetectiveLocation(Detective detective) {
+			return Optional.empty();
+		}
+
+		@Nonnull @Override
+		public Optional<TicketBoard> getPlayerTickets(Piece piece) {
+			return Optional.empty();
+		}
+
+		@Nonnull @Override
+		public ImmutableList<LogEntry> getMrXTravelLog() {
+			return log;
+		}
+
+		@Nonnull @Override
+		public ImmutableSet<Piece> getWinner() {
+			return winner;
+		}
+
+		@Nonnull @Override
+		public ImmutableSet<Move> getAvailableMoves() {
+			return null;
+		}
+
+		@Override public GameState advance(Move move) {  return null;  }
+
+	}
+
 	@Nonnull @Override public GameState build(
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
 
-
-		// PRE GAME STATE CREATION TESTS:
-		if (mrX == null) throw new NullPointerException();    				// check mrX not null
-		if (detectives.contains(null)) throw new NullPointerException();  	// check no detective is null
-		if (mrX.isDetective()) throw new IllegalArgumentException();   		// check mrX is not a detective
-		for (Player detective : detectives) { 								// check ticket allocation
-			if (detective.has(ScotlandYard.Ticket.SECRET) || detective.has(ScotlandYard.Ticket.DOUBLE)) throw new IllegalArgumentException();    }
-		if (mrX.has(ScotlandYard.Ticket.TAXI) || mrX.has(ScotlandYard.Ticket.BUS) || mrX.has(ScotlandYard.Ticket.UNDERGROUND)) throw new IllegalArgumentException();
-
-
-		// GAME STATE CREATION:
-		//GameState nextGameState = new GameState(); 						// contains 8 methods to implement
-		//return nextGameState;
-
-		throw new RuntimeException("Implement me!");
+		return new MyGameState(setup, ImmutableSet.of(MrX.MRX), ImmutableList.of(), mrX, detectives);
 
 	}
+
+
 
 }
